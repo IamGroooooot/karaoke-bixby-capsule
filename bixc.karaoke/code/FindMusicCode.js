88049fe24471b8a9a1d3ed769eb508b631e3c974
IamGroooooot <dury.ko@gmail.com>
2020-01-21 17:20:41 +0900

    Add - option 넣기, Update - UI

--- bixc.karaoke/code/FindMusicCode.js
+++ bixc.karaoke/code/FindMusicCode.js
@@ -2,20 +2,30 @@ var http = require('http')
 var console = require('console')
 var config = require('config')
 
-module.exports.function = function findMusicCode (songTitle, karaokeCompany) {
+module.exports.function = function findMusicCode (keyword, type, cond, karaokeCompany) {
   console.log("Find Music by a title")
-  
+  karaokeCompany = karaokeCompany.toLowerCase()
   //var options = {}
   var ret = []
 
-  // optional이므로 체크해야함
-  //if(typeof(songTitle) != "undefined"){
-  //  options["keyword"] = songTitle;
+  // optional이므로 체크해야 함
+  //if(typeof(keyword) != "undefined"){
+  //  options["keyword"] = keyword;
   //}
+  //if(typeof(type) != "undefined"){
+  //  options["type"] = type;
+  //}
+  //if(typeof(cond) != "undefined"){
+  //  options["cond"] = cond;
+  //}
+
+  
   var options = { 
     format: 'json',
     query: {
-      keyword: songTitle
+      keyword: keyword,
+      type: type,
+      cond: cond
     }
   }
 
@@ -26,13 +36,19 @@ module.exports.function = function findMusicCode (songTitle, karaokeCompany) {
   }
 
   try {
-    ret = http.getUrl(config.get('remote.url') + '/search', options);
+    //if(karaokeCompany.includes('tj')){
+      ret = http.getUrl(config.get('remote.url') + '/search', options);
+      //ret = http.getUrl(config.get('remote.url') + '/searchTJ', options);
+
+   // }else{
+    //  ret = http.getUrl(config.get('remote.url') + '/search', options);
+      //ret = http.getUrl(config.get('remote.url') + '/searchKY', options);
+
+   // }
     //console.log(res.title)
   } catch(err) {
     console.log("Error - " + err)
   }
-
-  console.log(ret)
 
   return ret.title
 
